33328bc9f60549eba9fd11417b820e2ac7da44ab
cdieckhaus <cdieckhaus@microsoft.com>
2022-04-22 16:12:10 -0700

    Intiail graphsage refactor.

--- examples/pytorch/graphsage/run.sh
+++ examples/pytorch/graphsage/run.sh
@@ -22,46 +22,30 @@ rm -rf $MODEL_DIR
 
 if [[ "${DEVICE}" == "gpu" ]]
 then
-    PLATFORM_DEVICE=--gpu
+    PLATFORM_DEVICE="--gpu=true"
 fi
 
 ### ===== training =======
-python ${DIR_NAME}/main.py  \
---data_dir $GRAPH --mode train --seed 123 \
---backend snark --graph_type local --converter skip \
---batch_size 140 --learning_rate 0.005 --num_epochs 100 \
---node_type 0 --max_id -1 \
---model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path $MODEL_DIR \
---feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo $ALGO \
---log_by_steps 1 --use_per_step_metrics $PLATFORM_DEVICE --storage_type $STORAGE_TYPE
+python ${DIR_NAME}/main.py --conf-dir conf \
+    --data_dir=$GRAPH --algo=$ALGO --model_dir=$MODEL_DIR \
+    --metric_dir=$MODEL_DIR --save_path=$MODEL_DIR $PLATFORM_DEVICE \
+    --storage_type=$STORAGE_TYPE
 
-python ${DIR_NAME}/main.py  \
---data_dir $GRAPH --mode evaluate \
---backend snark --graph_type local --converter skip \
---batch_size 1000 \
---sample_file /tmp/cora/test.nodes --node_type 0 --max_id -1 \
---feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo $ALGO \
---model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path $MODEL_DIR \
---log_by_steps 1 --use_per_step_metrics $PLATFORM_DEVICE
+python ${DIR_NAME}/main.py --conf-dir conf \
+    --mode=evaluate --batch_size=1000 --sample_file=/tmp/cora/test.nodes \
+    --data_dir=$GRAPH --algo=supervised --model_dir=$MODEL_DIR \
+    --metric_dir=$MODEL_DIR --save_path=$MODEL_DIR $PLATFORM_DEVICE
 
 if [[ "$ADL_UPLOADER" == "no" ]]; then
-    python ${DIR_NAME}/main.py  \
-    --data_dir $GRAPH --mode inference \
-    --backend snark --graph_type local --converter skip \
-    --batch_size 1000 \
-    --sample_file /tmp/cora/test.nodes --node_type 0 \
-    --feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo $ALGO \
-    --model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path $MODEL_DIR \
-    --log_by_steps 1 --use_per_step_metrics $PLATFORM_DEVICE
+    python ${DIR_NAME}/main.py --conf-dir conf \
+        --mode=inference --batch_size=1000 --sample_file=/tmp/cora/test.nodes \
+        --data_dir=$GRAPH --algo=$ALGO --model_dir=$MODEL_DIR \
+        --metric_dir=$MODEL_DIR --save_path=$MODEL_DIR $PLATFORM_DEVICE
 else
-    python ${DIR_NAME}/main.py  \
-    --data_dir $GRAPH --mode inference \
-    --backend snark --graph_type local --converter skip \
-    --batch_size 1000 \
-    --sample_file /tmp/cora/test.nodes --node_type 0 \
-    --feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo $ALGO \
-    --model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path /integration_test/test_adl_uploader \
-    --log_by_steps 1 --use_per_step_metrics --enable_adl_uploader --uploader_store_name snrgnndls --uploader_process_num 2 --uploader_threads_num 10 $PLATFORM_DEVICE
+    python ${DIR_NAME}/main.py --conf-dir conf +deepgnn=adl_uploader \
+        --mode=inference --batch_size=1000 --sample_file=/tmp/cora/test.nodes \
+        --data_dir=$GRAPH --algo=$ALGO --model_dir=$MODEL_DIR \
+        --metric_dir=$MODEL_DIR $PLATFORM_DEVICE
 fi
 
 rm -rf $MODEL_DIR
@@ -73,15 +57,10 @@ if [[ "$USE_HADOOP" == "yes" ]]; then
     else
         . ../../hdfs_setup.sh
     fi
-    python ${DIR_NAME}/main.py  \
-    --data_dir file://$GRAPH --mode train --seed 123 \
-    --backend snark --graph_type local --converter skip \
-    --batch_size 140 --learning_rate 0.005 --num_epochs 100 \
-    --node_type 0 --max_id -1 \
-    --model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path $MODEL_DIR \
-    --feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo $ALGO \
-    --log_by_steps 1 --use_per_step_metrics $PLATFORM_DEVICE \
-    --stream
+    python ${DIR_NAME}/main.py --conf-dir conf \
+        --data_dir=file://$GRAPH --algo=$ALGO --model_dir=$MODEL_DIR \
+        --metric_dir=$MODEL_DIR --save_path=$MODEL_DIR $PLATFORM_DEVICE \
+        --stream=true
 fi
 
 rm -rf $MODEL_DIR
