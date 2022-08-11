a1ebe1a04411e79f496c419047c1a34618f3bf08
cdieckhaus <cdieckhaus@microsoft.com>
2022-08-11 12:38:03 -0700

    thread_count -> parition_count.

--- src/python/deepgnn/graph_engine/snark/convert.py
+++ src/python/deepgnn/graph_engine/snark/convert.py
@@ -115,7 +115,7 @@ class MultiWorkersConverter:
 
         for data in dataset:
             data_len = len(data)
-            split_len = data_len // self.thread_count + 1
+            split_len = data_len // self.partition_count + 1
             for i in range(0, data_len, split_len):
                 d.dispatch(data[i : i + split_len])
 
