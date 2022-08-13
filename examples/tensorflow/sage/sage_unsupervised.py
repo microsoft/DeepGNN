f8f6b4acaef81a73e03e46a40e9c71aef4d2dc2d
cdieckhaus <cdieckhaus@microsoft.com>
2022-08-12 17:36:01 -0700

    Update.

--- examples/tensorflow/sage/sage_unsupervised.py
+++ examples/tensorflow/sage/sage_unsupervised.py
@@ -76,7 +76,9 @@ class UnsupervisedQuery(SAGEQuery):
         if self.param.identity_feature:
             graph_tensor = [all_nodes, src_idx, pos_idx, neg_idx]
         else:
-            feat = graph.node_features(all_nodes, self.feat_meta, self.param.dtype)
+            feat = graph.node_features(
+                all_nodes, self.feat_meta, self.param.feature_type
+            )
             graph_tensor = [all_nodes, feat, src_idx, pos_idx, neg_idx]
 
         graph_tensor.extend(neighbor_list_idx)
