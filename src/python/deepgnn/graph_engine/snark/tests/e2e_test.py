56a2c963d8a090ce612ed9c72048dd32e6e81e64
cdieckhaus <cdieckhaus@microsoft.com>
2022-08-15 16:50:44 -0700

    Update.

--- src/python/deepgnn/graph_engine/snark/tests/e2e_test.py
+++ src/python/deepgnn/graph_engine/snark/tests/e2e_test.py
@@ -1101,6 +1101,8 @@ def test_remote_client_missing_edge_string_features_graph_multiple_partitions(
     s2.reset()
 
 
+"""
+
 @pytest.mark.parametrize(
     "storage_type",
     [client.PartitionStorageType.memory, client.PartitionStorageType.disk],
@@ -1341,8 +1343,6 @@ def test_distributed_graph_node_types(multi_partition_graph_data, storage_type):
     npt.assert_equal(types, np.array([0, 2, 1], dtype=np.int32))
     s1.reset()
 
-
-"""
 
 @pytest.mark.parametrize(
     "storage_type",
