a42be33460234ebc3513a7a2efe55923d4fbdcb3
Tiantian Wang <tiantiaw@MININT-Q11EFU7.fareast.corp.microsoft.com>
2022-09-30 18:04:05 +0800

    Fix build break in windows.

--- src/cc/lib/utils/threadpool.h
+++ src/cc/lib/utils/threadpool.h
@@ -23,8 +23,7 @@ class ThreadPool
     ThreadPool();
     ThreadPool(size_t);
 
-    template <class F, class... Args>
-    auto Submit(F &&f, Args &&...args) -> std::future<typename std::result_of<F(Args...)>::type>;
+    template <class F, class... Args> decltype(auto) Submit(F &&f, Args &&...args);
 
     ~ThreadPool();
 
@@ -80,10 +79,9 @@ inline void ThreadPool::Initialize(size_t threads)
     }
 }
 
-template <class F, class... Args>
-auto ThreadPool::Submit(F &&f, Args &&...args) -> std::future<typename std::result_of<F(Args...)>::type>
+template <class F, class... Args> decltype(auto) ThreadPool::Submit(F &&f, Args &&...args)
 {
-    using return_type = typename std::result_of<F(Args...)>::type;
+    using return_type = decltype(f(args...));
 
     auto task =
         std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
