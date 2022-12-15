17e725d8dae39a8d9f721738186a8aaf84bd5875
cdieckhaus <cdieckhaus@microsoft.com>
2022-12-14 17:42:25 -0800

    Cleanup.

--- src/python/deepgnn/migrate/data/ray_util.py
+++ src/python/deepgnn/migrate/data/ray_util.py
@@ -1,72 +1,49 @@
+"""Baseline Ray Trainer."""
 from typing import Dict
-import argparse
 import platform
-import torch
-from typing import Optional, Callable, List
-from deepgnn import TrainerType
-from deepgnn import get_logger
-from contextlib import closing
 import numpy as np
-import torch.nn as nn
+import torch
 import ray
 import ray.train as train
 from ray.train.torch import TorchTrainer
 from ray.air import session
-from ray.air.config import ScalingConfig, RunConfig
-from deepgnn.pytorch.common import init_common_args
-from deepgnn.pytorch.training.args import init_trainer_args, init_fp16_args
-from deepgnn.pytorch.training.trainer import Trainer
+from ray.air.config import ScalingConfig
+from deepgnn import TrainMode
 from deepgnn.graph_engine import create_backend, BackendOptions
 from deepgnn.graph_engine.samplers import GENodeSampler, GEEdgeSampler
-from deepgnn.graph_engine.snark.local import Client
-
-
-# TODO put in util
-def get_args(init_arg_fn: Optional[Callable] = None, run_args: Optional[List] = None):
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser(allow_abbrev=False)
-
-    # Initialize common parameters, including model, dataset, optimizer etc.
-    init_common_args(parser)
-
-    # Initialize trainer paramaters.
-    init_trainer_args(parser)
-
-    # Initialize fp16 related paramaters.
-    init_fp16_args(parser)
-
-    if init_arg_fn is not None:
-        init_arg_fn(parser)
-
-    args, _ = parser.parse_known_args() if run_args is None else parser.parse_known_args(run_args)
-    for arg in dir(args):
-        if not arg.startswith("_"):
-            get_logger().info(f"{arg}={getattr(args, arg)}")
-
-    return args
+from deepgnn.pytorch.common import get_args
 
 
 def train_func(config: Dict):
+    """Training loop for ray trainer."""
     train.torch.enable_reproducibility(seed=session.get_world_rank())
 
     args = config["args"]
 
+    if args.fp16:
+        train.accelerate(True)
+
     model = config["init_model_fn"](args)
-    model = train.torch.prepare_model(model)  # TODO any relevant args
-    model.train()
+    model = train.torch.prepare_model(model, move_to_device=args.gpu)
+    if args.mode == TrainMode.TRAIN:
+        model.train()
+    else:
+        model.eval()
 
     optimizer = config["init_optimizer_fn"](
         args,
         model,
         session.get_world_size(),
     )
-    optimizer = train.torch.prepare_optimizer(optimizer)  # TODO any relevant args
+    optimizer = train.torch.prepare_optimizer(optimizer)
 
-    backend = create_backend(BackendOptions(args), is_leader=(session.get_local_rank() == 0))  # TODO local or world
+    backend = create_backend(
+        BackendOptions(args), is_leader=(session.get_world_rank() == 0)
+    )
     dataset = config["init_dataset_fn"](
         args,
         model,
-        rank=session.get_local_rank(),  # TODO local or world
+        rank=session.get_world_rank(),
         world_size=session.get_world_size(),
         backend=backend,
     )
@@ -94,8 +71,8 @@ def train_func(config: Dict):
             labels.append(label)
             losses.append(loss.item())
 
-            #if i >= SAMPLE_NUM / BATCH_SIZE / session.get_world_size():
-            #    break
+            if i >= len(dataset) / args.batch_size / session.get_world_size():
+                break
 
         session.report(
             {
@@ -105,25 +82,25 @@ def train_func(config: Dict):
         )
 
 
-def run_ray(
-    init_model_fn,
-    init_dataset_fn,
-    init_optimizer_fn,
-    init_args_fn,
-    **kwargs
-):
+def run_ray(init_model_fn, init_dataset_fn, init_optimizer_fn, init_args_fn, **kwargs):
+    """Run ray trainer."""
     ray.init()
+
+    args = get_args(
+        init_args_fn, kwargs["run_args"] if "run_args" in kwargs else None
+    )
 
     trainer = TorchTrainer(
         train_func,
         train_loop_config={
-            "args": get_args(init_args_fn, kwargs["run_args"] if "run_args" in kwargs else None),
+            "args": args,
             "init_model_fn": init_model_fn,
             "init_dataset_fn": init_dataset_fn,
             "init_optimizer_fn": init_optimizer_fn,
-            **kwargs
+            **kwargs,
         },
-        run_config=RunConfig(),
-        scaling_config=ScalingConfig(num_workers=1, use_gpu=False, resources_per_worker={"CPU": 2}),
+        scaling_config=ScalingConfig(
+            num_workers=1, use_gpu=args.gpu, resources_per_worker={"CPU": 2}
+        ),
     )
-    result = trainer.fit()
+    trainer.fit()
