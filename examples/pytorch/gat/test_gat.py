44160a2bab4ef33965cc1488c90c88f09b096ef4
cdieckhaus <cdieckhaus@microsoft.com>
2022-12-20 13:10:46 -0800

    Add GAT test.

--- examples/pytorch/gat/test_gat.py
+++ examples/pytorch/gat/test_gat.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT License.
 
 import os
+import platform
 import sys
 import pytest
 import tempfile
@@ -9,7 +10,6 @@ import numpy as np
 import torch
 import argparse
 
-from deepgnn.pytorch.common.utils import set_seed
 from deepgnn.pytorch.common.dataset import TorchDeepGNNDataset
 from deepgnn.graph_engine import (
     GraphType,
@@ -22,38 +22,91 @@ from deepgnn.graph_engine.snark.converter.options import DataConverterType
 from deepgnn.graph_engine.data.citation import Cora
 
 from model_geometric import GAT, GATQueryParameter  # type: ignore
+from main import init_args, create_model, create_dataset, create_optimizer
+from ray_util import run_ray
 from deepgnn import get_logger
 
 
-def setup_test(main_file):
-    tmp_dir = tempfile.TemporaryDirectory()
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    mainfile = os.path.join(current_dir, main_file)
-    return tmp_dir, tmp_dir.name, tmp_dir.name, mainfile
+def setup_module(module):
+    import deepgnn.graph_engine.snark._lib as lib
+
+    lib_name = "libwrapper.so"
+    if platform.system() == "Windows":
+        lib_name = "wrapper.dll"
+
+    os.environ[lib._SNARK_LIB_PATH_ENV_KEY] = os.path.join(
+        os.path.dirname(__file__), "..", "..", "..", "src", "cc", "lib", lib_name
+    )
 
 
-def test_pytorch_gat_cora():
-    set_seed(123)
-    g = Cora()
+@pytest.fixture(scope="module")
+def train_graphsage_cora_ddp_trainer():
+    model_dir = tempfile.TemporaryDirectory()
+    working_dir = tempfile.TemporaryDirectory()
+    Cora(working_dir.name)
+
+    result = run_ray(
+        init_model_fn=create_model,
+        init_dataset_fn=create_dataset,
+        init_optimizer_fn=create_optimizer,
+        init_args_fn=init_args,
+        run_args=[
+            "--data_dir", working_dir.name,
+            "--mode", "train",
+            "--seed", "123",
+            "--backend", "snark",
+            "--graph_type", "local",
+            "--converter skip",
+            "--batch_size", "140",
+            "--learning_rate", "0.005",
+            "--num_epochs", "200",
+            "--node_type", "0",
+            "--max_id", "-1",
+            "--model_dir", model_dir.name,
+            "--metric_dir", model_dir.name,
+            "--save_path", model_dir.name,
+            "--feature_idx", "0",
+            "--feature_dim", "1433",
+            "--label_idx", "1",
+            "--label_dim", "1",
+            "--algo", "supervised",
+            "--neighbor_edge_types", "0",
+            "--in_dim", "1433",
+            "--head_num", "8,1",
+            "--hidden_dim", "8",
+            "--num_classes", "7",
+            "--ffd_drop", "0.6",
+            "--attn_drop", "0.6",
+            "--sample_file", f"{working_dir.name}/train.nodes",
+        ],
+    )
+    yield {"model_path": os.path.join(model_dir.name, 'gnnmodel-196-000000.pt'), "data_dir": working_dir.name}
+    working_dir.cleanup()
+    model_dir.cleanup()
+
+
+def test_pytorch_gat_cora(train_graphsage_cora_ddp_trainer):
     qparam = GATQueryParameter(
         neighbor_edge_types=np.array([0], np.int32),
         feature_idx=0,
-        feature_dim=g.FEATURE_DIM,
+        feature_dim=1433,
         label_idx=1,
         label_dim=1,
     )
     model = GAT(
-        in_dim=g.FEATURE_DIM,
+        in_dim=1433,
         head_num=[8, 1],
         hidden_dim=8,
-        num_classes=g.NUM_CLASSES,
+        num_classes=7,
         ffd_drop=0.6,
         attn_drop=0.6,
         q_param=qparam,
     )
 
+    model.load_state_dict(torch.load(train_graphsage_cora_ddp_trainer["model_path"])["state_dict"])
+
     args = argparse.Namespace(
-        data_dir=g.data_dir(),
+        data_dir=train_graphsage_cora_ddp_trainer["data_dir"],
         backend=BackendType.SNARK,
         graph_type=GraphType.LOCAL,
         converter=DataConverterType.SKIP,
@@ -61,44 +114,6 @@ def test_pytorch_gat_cora():
     )
 
     backend = create_backend(BackendOptions(args), is_leader=True)
-
-    def create_dataset():
-        ds = TorchDeepGNNDataset(
-            sampler_class=FileNodeSampler,
-            backend=backend,
-            query_fn=model.q.query_training,
-            prefetch_queue_size=1,
-            prefetch_worker_size=1,
-            sample_files=os.path.join(g.data_dir(), "train.nodes"),
-            batch_size=140,
-            shuffle=True,
-            drop_last=True,
-            worker_index=0,
-            num_workers=1,
-        )
-
-        return torch.utils.data.DataLoader(ds)
-
-    optimizer = torch.optim.Adam(
-        filter(lambda p: p.requires_grad, model.parameters()),
-        lr=0.005,
-        weight_decay=0.0005,
-    )
-
-    # train
-    num_epochs = 200
-    ds = create_dataset()
-    model.train()
-    for ei in range(num_epochs):
-        for si, batch_input in enumerate(ds):
-            optimizer.zero_grad()
-            loss, pred, label = model(batch_input)
-            loss.backward()
-            optimizer.step()
-            acc = model.compute_metric([pred], [label])
-            get_logger().info(
-                f"epoch {ei} - {si}, loss {loss.data.item() :.6f}, accuracy {acc.data.item():.6f}"
-            )
 
     # evaluate
     def create_eval_dataset():
@@ -109,7 +124,7 @@ def test_pytorch_gat_cora():
             prefetch_queue_size=1,
             prefetch_worker_size=1,
             batch_size=1000,
-            sample_files=os.path.join(g.data_dir(), "test.nodes"),
+            sample_files=os.path.join(train_graphsage_cora_ddp_trainer["data_dir"], "test.nodes"),
             shuffle=False,
             drop_last=True,
             worker_index=0,
