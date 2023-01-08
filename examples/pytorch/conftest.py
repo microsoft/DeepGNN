1429f02411e4781b7cccc30518ea6dfc427814c0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-08 15:10:10 +0000

    Adding tarfile member sanitization to extractall()

--- examples/pytorch/conftest.py
+++ examples/pytorch/conftest.py
@@ -22,7 +22,26 @@ def prepare_local_test_files():
         f"https://deepgraphpub.blob.core.windows.net/public/testdata/{name}", zip_file
     )
     with tarfile.open(zip_file) as tar:
-        tar.extractall(working_dir.name)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tar, working_dir.name)
 
     yield working_dir.name
     working_dir.cleanup()
