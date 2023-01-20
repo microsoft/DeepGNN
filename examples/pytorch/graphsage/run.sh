1b40e427afbc5a8e6f8899cc74b98f014b68e643
cdieckhaus <cdieckhaus@microsoft.com>
2023-01-19 17:08:40 -0800

    Update.

--- examples/pytorch/graphsage/run.sh
+++ examples/pytorch/graphsage/run.sh
@@ -33,55 +33,53 @@ python ${DIR_NAME}/main.py  \
 --feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo supervised \
 --log_by_steps 1 --use_per_step_metrics $PLATFORM_DEVICE --storage_type $STORAGE_TYPE
 
+python ${DIR_NAME}/main.py  \
+--data_dir $GRAPH --mode evaluate \
+--backend snark --graph_type local --converter skip \
+--batch_size 1000 \
+--sample_file /tmp/cora/test.nodes --node_type 0 --max_id -1 \
+--feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo supervised \
+--model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path $MODEL_DIR \
+--log_by_steps 1 --use_per_step_metrics $PLATFORM_DEVICE
 
+if [[ "$ADL_UPLOADER" == "no" ]]; then
+    python ${DIR_NAME}/main.py  \
+    --data_dir $GRAPH --mode inference \
+    --backend snark --graph_type local --converter skip \
+    --batch_size 1000 \
+    --sample_file /tmp/cora/test.nodes --node_type 0 \
+    --feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo supervised \
+    --model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path $MODEL_DIR \
+    --log_by_steps 1 --use_per_step_metrics $PLATFORM_DEVICE
+else
+    python ${DIR_NAME}/main.py  \
+    --data_dir $GRAPH --mode inference \
+    --backend snark --graph_type local --converter skip \
+    --batch_size 1000 \
+    --sample_file /tmp/cora/test.nodes --node_type 0 \
+    --feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo supervised \
+    --model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path /integration_test/test_adl_uploader \
+    --log_by_steps 1 --use_per_step_metrics --enable_adl_uploader --uploader_store_name snrgnndls --uploader_process_num 2 --uploader_threads_num 10 $PLATFORM_DEVICE
+fi
 
-#python ${DIR_NAME}/main.py  \
-#--data_dir $GRAPH --mode evaluate \
-#--backend snark --graph_type local --converter skip \
-#--batch_size 1000 \
-#--sample_file /tmp/cora/test.nodes --node_type 0 --max_id -1 \
-#--feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo supervised \
-#--model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path $MODEL_DIR \
-#--log_by_steps 1 --use_per_step_metrics $PLATFORM_DEVICE
-
-#if [[ "$ADL_UPLOADER" == "no" ]]; then
-#    python ${DIR_NAME}/main.py  \
-#    --data_dir $GRAPH --mode inference \
-#    --backend snark --graph_type local --converter skip \
-#    --batch_size 1000 \
-#    --sample_file /tmp/cora/test.nodes --node_type 0 \
-#    --feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo supervised \
-#    --model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path $MODEL_DIR \
-#    --log_by_steps 1 --use_per_step_metrics $PLATFORM_DEVICE
-#else
-#    python ${DIR_NAME}/main.py  \
-#    --data_dir $GRAPH --mode inference \
-#    --backend snark --graph_type local --converter skip \
-#    --batch_size 1000 \
-#    --sample_file /tmp/cora/test.nodes --node_type 0 \
-#    --feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo supervised \
-#    --model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path /integration_test/test_adl_uploader \
-#    --log_by_steps 1 --use_per_step_metrics --enable_adl_uploader --uploader_store_name snrgnndls --uploader_process_num 2 --uploader_threads_num 10 $PLATFORM_DEVICE
-#fi
-
-#rm -rf $MODEL_DIR
+rm -rf $MODEL_DIR
 
 # HDFS example
-#if [[ "$USE_HADOOP" == "yes" ]]; then
-#    if [[ -d "tools" ]]; then
-#        . tools/azurepipeline/hdfs_setup.sh
-#    else
-#        . ../../hdfs_setup.sh
-#    fi
-#    python ${DIR_NAME}/main.py  \
-#    --data_dir file://$GRAPH --mode train --seed 123 \
-#    --backend snark --graph_type local --converter skip \
-#    --batch_size 140 --learning_rate 0.005 --num_epochs 100 \
-#    --node_type 0 --max_id -1 \
-#    --model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path $MODEL_DIR \
-#    --feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo supervised \
-#    --log_by_steps 1 --use_per_step_metrics $PLATFORM_DEVICE \
-#    --stream
-#fi
+if [[ "$USE_HADOOP" == "yes" ]]; then
+    if [[ -d "tools" ]]; then
+        . tools/azurepipeline/hdfs_setup.sh
+    else
+        . ../../hdfs_setup.sh
+    fi
+    python ${DIR_NAME}/main.py  \
+    --data_dir file://$GRAPH --mode train --seed 123 \
+    --backend snark --graph_type local --converter skip \
+    --batch_size 140 --learning_rate 0.005 --num_epochs 100 \
+    --node_type 0 --max_id -1 \
+    --model_dir $MODEL_DIR --metric_dir $MODEL_DIR --save_path $MODEL_DIR \
+    --feature_idx 1 --feature_dim 50 --label_idx 0 --label_dim 121 --algo supervised \
+    --log_by_steps 1 --use_per_step_metrics $PLATFORM_DEVICE \
+    --stream
+fi
 
 rm -rf $MODEL_DIR
