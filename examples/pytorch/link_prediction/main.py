843a9d4b1cc3a1ff185f45ac6b16764ff8dbf770
cdieckhaus <cdieckhaus@microsoft.com>
2023-02-14 17:38:36 -0800

    Broken link pred and knowledgegraph .

--- examples/pytorch/link_prediction/main.py
+++ examples/pytorch/link_prediction/main.py
@@ -1,6 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-
+from typing import Dict
 import argparse
 import os
 import torch.optim
@@ -20,69 +20,95 @@ from args import init_args  # type: ignore
 from consts import DEFAULT_VOCAB_CHAR_INDEX  # type: ignore
 from model import LinkPredictionModel  # type: ignore
 from deepgnn.graph_engine.snark.distributed import Client as DistributedClient
+from ray.train.torch import TorchTrainer
+from ray.air import session
+from ray.air.config import ScalingConfig, RunConfig
+import ray.train as train
+from deepgnn.graph_engine.data.citation import Cora
+from deepgnn.graph_engine.snark.distributed import Server, Client as DistributedClient
+from deepgnn.pytorch.common import get_args
 
 
-def create_model(args: argparse.Namespace):
-    get_logger().info(f"Creating LinkPredictionModel with seed:{args.seed}.")
-    # set seed before instantiating the model
+def train_func(config: Dict):
+    """Training loop for ray trainer."""
+    args = config["args"]
+
+    train.torch.accelerate()
+    train.torch.enable_reproducibility(seed=session.get_world_rank())
 
     feature_enc = get_feature_encoder(args)
-
-    return LinkPredictionModel(
+    model = LinkPredictionModel(
         args=args,
-        feature_dim=args.feature_dim,
-        feature_idx=args.feature_idx,
-        feature_type=get_python_type(args.feature_type),
+        feature_dim=config["feature_dim"],
+        feature_idx=config["feature_idx"],
+        feature_type=np.float32,
         feature_enc=feature_enc[0],  # type: ignore
         vocab_index=feature_enc[1][DEFAULT_VOCAB_CHAR_INDEX],  # type: ignore
     )
+    model = train.torch.prepare_model(model)
+    model.train()
 
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=0.005,
+    )
+    optimizer = train.torch.prepare_optimizer(optimizer)
 
-def create_dataset(
-    args: argparse.Namespace,
-    model: BaseModel,
-    rank: int = 0,
-    world_size: int = 1,
-    address: str = "",
-) -> ray.data.DatasetPipeline:
-    g = DistributedClient([address])
-    max_id = g.node_count(args.node_type) if args.max_id in [-1, None] else args.max_id
+    g = DistributedClient(config["ge_address"])
+    max_id = g.node_count(0)
     dataset = ray.data.range(max_id).repartition(max_id // args.batch_size)
     pipe = dataset.window(blocks_per_window=4).repeat(args.num_epochs)
+    pipe = pipe.map_batches(lambda idx: model.q.query_training(g, np.array(idx)))
 
-    def transform_batch(idx: list) -> dict:
-        return model.q.query_training(g, np.array(idx))
+    for epoch, epoch_pipe in enumerate(pipe.iter_epochs()):
+        scores = []
+        labels = []
+        losses = []
+        for step, batch in enumerate(
+            epoch_pipe.iter_torch_batches(batch_size=args.batch_size)
+        ):
+            loss, score, label = model(batch)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-    pipe = pipe.map_batches(transform_batch)
-    return pipe
+            scores.append(score)
+            labels.append(label)
+            losses.append(loss.item())
 
-
-def create_optimizer(args: argparse.Namespace, model: BaseModel, world_size: int):
-    return torch.optim.AdamW(
-        model.parameters(),
-        lr=args.learning_rate * world_size,
-        weight_decay=args.weight_decay,
-    )
+        session.report(
+            {
+                "metric": model.compute_metric(scores, labels).item(),
+                "loss": np.mean(losses),
+            },
+        )
 
 
 def _main():
-    # setup default logging component.
     setup_default_logging_config(enable_telemetry=True)
 
-    # run_dist is the unified entry for pytorch model distributed training/evaluation/inference.
-    # User only needs to prepare initializing function for model, dataset, optimizer and args.
-    # reference: `deepgnn/pytorch/training/factory.py`
     ray.init(num_cpus=4)
+
+    address = "localhost:9999"
+    cora = Cora()
+    s = Server(address, cora.data_dir(), 0, 1)
+    training_loop_config = {
+        "ge_address": address,
+        "batch_size": 140,
+        "num_epochs": 200,
+        "feature_idx": 1,
+        "feature_dim": 50,
+        "label_idx": 0,
+        "label_dim": 121,
+        "num_classes": 7,
+        "learning_rate": 0.005,
+        "args": get_args(),
+    }
 
     trainer = TorchTrainer(
         train_func,
-        train_loop_config={
-            "init_model_fn": create_model,
-            "init_dataset_fn": create_dataset,
-            "init_optimizer_fn": create_optimizer,
-            **kwargs,
-        },
-        scaling_config=ScalingConfig(num_workers=1, use_gpu=args.gpu),
+        train_loop_config=training_loop_config,
+        scaling_config=ScalingConfig(num_workers=1),
     )
     return trainer.fit()
 
