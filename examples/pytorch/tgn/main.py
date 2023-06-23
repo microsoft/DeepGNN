aafd394364674ad78799df724a323105095563a3
Alex Samylkin <alsamylk@microsoft.com>
2023-06-23 16:04:51 -0700

    Remove kg and start tgn

---
+++ examples/pytorch/tgn/main.py
@@ -0,0 +1,8 @@
+from graph import prepare_data
+
+if __name__ == "__main__":
+    print("main started")
+    # prepare_data("reddit")
+    # prepare_data("wikipedia")
+    # prepare_data("mooc")
+    # prepare_data("lastfm")
